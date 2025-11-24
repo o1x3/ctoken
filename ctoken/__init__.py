3a691b3602d5d1fdfa6873d6f54d2f5fba9392a2
Karthik Vinayan <karthikdoestech@gmail.com>
2025-11-25 01:01:03 +0530

    release: bump version to 1.1.0

--- ctoken/__init__.py
+++ ctoken/__init__.py
@@ -22,7 +22,7 @@ from .token_estimator import estimate_openai_api_cost as estimate_api_cost
 # Create alias for the main function
 ctoken = estimate_api_cost
 
-__version__ = "1.0.5"
+__version__ = "1.1.0"
 
 __all__ = [
     "calculate_cost",
